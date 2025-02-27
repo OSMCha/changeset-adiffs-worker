a9fb5285856e3fd993999351c2f667bfb2a08618
Jake Low <hello@jakelow.com>
2025-02-27 13:43:30 -0800

    Post tag changes to OSMCha Django backend
    By deploying this, we're enabling the new service in production.

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,46 @@ import parseAugmentedDiff from "@osmcha/osm-adiff-parser";
 import changetags from "./changetags.js";
 
 type Environment = {
+  // bindings
   readonly CHANGESET_ADIFFS: Queue;
-  BUCKET: R2Bucket;
+  readonly BUCKET: R2Bucket;
+  // env vars
+  readonly OSMCHA_API_URL: string;
+  readonly OSMCHA_ADMIN_TOKEN: string;
 };
 
 export default {
   async queue(batch: MessageBatch<any>, env: Environment): Promise<void> {
-    // Do something with messages in the batch
-    // i.e. write to R2 storage, D1 database, or POST to an external API
-    // You can also iterate over each message in the batch by looping over batch.messages
     for (const message of batch.messages) {
-      let res = await env.BUCKET.get(message.body.object.key);
-      let decompressed = res.body.pipeThrough(new DecompressionStream("gzip"));
-      let xmlString = await (new Response(decompressed)).text();
-      let adiff = await parseAugmentedDiff(xmlString);
-      let changes = changetags(adiff);
+      try {
+        // Fetch and decompress the augmented diff file from R2
+        let r2object = await env.BUCKET.get(message.body.object.key);
+        let decompressed = r2object.body.pipeThrough(new DecompressionStream("gzip"));
+        let xmlString = await (new Response(decompressed)).text();
 
-      // TODO: post the tag changes to osmcha.org/api/v1/changesets/:id/tag-changes/
-      // ...for now, just log them
-      console.log(message.body.object.key, JSON.stringify(Object.fromEntries(changes.entries().map(([k, v]) => [k, Array.from(v)]))));
+        // Parse the adiff file and compute the tag changes
+        let adiff = await parseAugmentedDiff(xmlString);
+        let changes = changetags(adiff);
+        let plainChanges = Object.fromEntries(changes.entries().map(([k, v]) => [k, Array.from(v)]));
+
+        // Post the changes to the OSMCha Django backend
+        let id = message.body.object.key.split("/").reverse()[0].split(".")[0];
+        let url = `${env.OSMCHA_API_URL}/changesets/${id}/tag-changes/`;
+        let res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Authorization": `Token ${env.OSMCHA_ADMIN_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(plainChanges),
+        });
+
+        if (!res.ok) {
+          throw new Error(`POST ${url} -> HTTP ${res.status} (body: ${await res.text()})`);
+        }
+      } catch (err) {
+        console.error(err);
+      }
     }
   },
 };
